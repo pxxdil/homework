82840b82e66f82ca383e7eada723cc043b35ef41
pxxdil <fdhilrzqi@gmail.com>
2022-03-25 16:35:36 +0700

    Update HW 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import data from './simple/simple.js'
-import {AlbumImage, AlbumName, ButtonUrl, AlbumDesc} from './components/somecomponent/index.js'
+import {AlbumGambar, AlbumName, ButtonUrl, AlbumDesc} from './components/somecomponent/index.js'
 
 function App() {
   return (
     <div className="App">
-      <AlbumImage src={data.album.images[0].url}>
+      <AlbumGambar src={data.album.images[1].url}>
         <AlbumDesc name={data.name}/>
-      </AlbumImage>
+      </AlbumGambar>
       <AlbumName name={data.album.name} artist={data.artists[0].name}>
         < ButtonUrl url={data.album.artists[0].external_urls.spotify} />
       </ AlbumName>
--- src/components/somecomponent/index.js
+++ src/components/somecomponent/index.js
@@ -1,21 +1,20 @@
-const AlbumImage = ({ src, children }) => (
-    <div>
-      <img src={src} width="300" height="300" className="rotate" />
-      {children}
-    </div>
-  );
-  
-  const AlbumDesc = (props) => <h2>Album:{props.name}</h2>;
-  
-  const AlbumName = ({name, artist, children}) => (
-    <div className="AlbumName">
-      <p>{name}</p>
-      <p>{artist}</p>
-      {children}
-    </div>
-  );
-  
-  const ButtonUrl = (url) => <button><a href={url}>select</a></button>;
-  
-  export {AlbumImage, AlbumDesc, AlbumName, ButtonUrl};
-  
+const AlbumGambar = ({ src, children }) => (
+  <div>
+    <img src={src} width="300" height="300" className="rotate" />
+    {children}
+  </div>
+);
+
+const AlbumDesc = (props) => <h2>Album:{props.name}</h2>;
+
+const AlbumName = ({name, artist, children}) => (
+  <div className="AlbumName">
+    <p>{name}</p>
+    <p>{artist}</p>
+    {children}
+  </div>
+);
+
+const ButtonUrl = (url) => <button className="btn"><a href={url}>select</a></button>;
+
+export {AlbumGambar, AlbumDesc, AlbumName, ButtonUrl};
